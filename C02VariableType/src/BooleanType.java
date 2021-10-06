eb2af689b3cfc317377d28b10d7431b413f7c934
Berreon <87958196+Berreon@users.noreply.github.com>
2021-10-06 16:39:06 +0900

    Add files via upload

---
+++ C02VariableType/src/BooleanType.java
@@ -0,0 +1,20 @@
+/*
+ * 논리타입
+ * 논리값
+ * 참과 거짓
+ */
+public class BooleanType {
+
+	public static void main(String[] args) {
+		
+		boolean t = true;
+		boolean f = false;
+		
+		System.out.println("참:"+ t);
+		System.out.println("거짓:"+ f);
+		
+		
+
+	}
+
+}
